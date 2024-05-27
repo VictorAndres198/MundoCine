704033178a0c79593ee1c37de56217ce6eb01fc6
Raul_LA <rleonalvarez090@gmail.com>
2024-05-26 22:54:01 -0500

    (+) clase para test unitarios

---
+++ src/test/java/Tests/Tests.java
@@ -0,0 +1,34 @@
+
+package Tests;
+
+import Services.ServiceDulceria;
+import conexion.ConectaBD;
+import java.sql.Connection;
+import java.util.List;
+import modelo.dto.itemDulceria;
+import org.junit.jupiter.api.Test;
+
+
+public class Tests {
+   
+   @Test
+    public void testConnection() {
+        Connection cnx = new ConectaBD().getConnection();
+        if(cnx != null){
+            System.out.println("Connection: OK");
+        }else{
+            System.out.println("Connection: ERROR");
+            
+        }
+    }
+
+    @Test
+    public void testDulceria() {
+        ServiceDulceria servDulceria = new ServiceDulceria();
+         List<itemDulceria> itemsDulceria = servDulceria.FindAll();
+         itemsDulceria.forEach(System.out::println);
+    }
+    
+    
+    
+}
