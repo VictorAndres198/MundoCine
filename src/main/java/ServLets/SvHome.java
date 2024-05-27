4939bab872e9103ae6e2f751489a1b447dfbaccb
VictorAndres198 <andresroh98@hotmail.com>
2024-05-27 12:31:45 -0500

    Avanzando la parte de peliculas en cartelera

--- src/main/java/ServLets/SvHome.java
+++ src/main/java/ServLets/SvHome.java
@@ -1,85 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package ServLets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelo.dto.Movie;
 
-/**
- *
- * @author aldom
- */
 public class SvHome extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SvHome</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SvHome at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        // Simular una lista de películas
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("Arthur", "resources/img/home-proximamente/proximamente1.jpg", "8.5"));
+        movies.add(new Movie("Culpa tuya", "resources/img/home-proximamente/proximamente2.webp", "7.2"));
+        movies.add(new Movie("Arthur", "resources/img/home-proximamente/proximamente1.jpg", "8.5"));
+        movies.add(new Movie("Culpa tuya", "resources/img/home-proximamente/proximamente2.webp", "7.2"));
+        movies.add(new Movie("Arthur", "resources/img/home-proximamente/proximamente1.jpg", "8.5"));
+        movies.add(new Movie("Culpa tuya", "resources/img/home-proximamente/proximamente2.webp", "7.2"));        
+        movies.add(new Movie("Arthur", "resources/img/home-proximamente/proximamente1.jpg", "8.5"));
+        movies.add(new Movie("Culpa tuya", "resources/img/home-proximamente/proximamente2.webp", "7.2"));
+        // Añade más películas según sea necesario
+
+        // Pasar la lista de películas al JSP
+        request.setAttribute("movies", movies);
+
+        // Redirigir al JSP
+        request.getRequestDispatcher("home.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-
+    }
+    
 }
