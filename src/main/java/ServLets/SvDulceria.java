85159a21ade324bed9b5caf852f185de1559a938
Raul_LA <rleonalvarez090@gmail.com>
2024-05-28 03:13:29 -0500

    (+) la busqueda de locales al Svlet Dulceria

--- src/main/java/ServLets/SvDulceria.java
+++ src/main/java/ServLets/SvDulceria.java
@@ -2,6 +2,7 @@
 package ServLets;
 
 import Services.ServiceDulceria;
+import Services.ServiceLocales;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import modelo.dto.Locales;
 import modelo.dto.itemDulceria;
 
 
@@ -22,10 +24,13 @@ public class SvDulceria extends HttpServlet {
             throws ServletException, IOException {
         
         ServiceDulceria servDulceria = new ServiceDulceria();
+        ServiceLocales servLocales = new ServiceLocales();
         
         List<itemDulceria> itemsDulceria = servDulceria.FindAll();
+        List<Locales> locales = servLocales.FindAll();
         
         request.setAttribute("itemsDulceria", itemsDulceria);
+        request.setAttribute("locales", locales);
         
         response.setContentType("text/html;charset=UTF-8");
         getServletContext().getRequestDispatcher("/dulceria.jsp").include(request, response);
