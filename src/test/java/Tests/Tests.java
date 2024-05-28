67eaa4bfe74e8399f4a83cbbbdcdacea779b4861
Raul_LA <rleonalvarez090@gmail.com>
2024-05-28 03:14:07 -0500

    (+) test del busqueda Locales

--- src/test/java/Tests/Tests.java
+++ src/test/java/Tests/Tests.java
@@ -2,15 +2,17 @@
 package Tests;
 
 import Services.ServiceDulceria;
+import Services.ServiceLocales;
+import conexion.ConectaBD;
 import java.sql.Connection;
 import java.util.List;
+import modelo.dto.Locales;
 import modelo.dto.itemDulceria;
 import org.junit.jupiter.api.Test;
-/*import conexion.ConectaBD;*/
 
 
 public class Tests {
-   /*
+
    @Test
     public void testConnection() {
         Connection cnx = new ConectaBD().getConnection();
@@ -22,10 +24,20 @@ public class Tests {
         }
     }
 
-    /*@Test
+    @Test
     public void testDulceria() {
         ServiceDulceria servDulceria = new ServiceDulceria();
          List<itemDulceria> itemsDulceria = servDulceria.FindAll();
          itemsDulceria.forEach(System.out::println);
-    }*/
+    
+    }
+    
+    @Test
+    public void testLocales() {
+        ServiceLocales servLocales = new ServiceLocales();
+         List<Locales> itemsDulceria = servLocales.FindAll();
+         itemsDulceria.forEach(System.out::println);
+    
+    }
+    
 }
